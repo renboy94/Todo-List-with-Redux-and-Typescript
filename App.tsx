a3f876826c495d9b251cf0bff127a7f44de4e8fa
Rendell Lasola <rendelllasola@gmail.com>
2020-04-15 01:43:46 +0800

    removed unused codes and comments

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  Alert,
-} from 'react-native';
-// import {createStore} from 'redux';
+import React from 'react';
 import {Provider} from 'react-redux';
-// import {reducers} from './src/reducers';
 import {PersistGate} from 'redux-persist/integration/react';
 import configStore from './configureStore';
 
 import Main from './src/components/Main';
-
-// const store = createStore(reducers);
 
 const App: React.FC = () => {
   return (
